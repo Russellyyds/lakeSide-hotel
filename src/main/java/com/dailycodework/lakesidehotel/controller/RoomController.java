6ce3afcb11449cf1b04301d40d5a58cb13731c20
Russellyyds <954013384@qq.com>
2024-01-25 21:49:55 +1100

    first commit

---
+++ src/main/java/com/dailycodework/lakesidehotel/controller/RoomController.java
@@ -0,0 +1,2 @@
+package com.dailycodework.lakesidehotel.controller;public class RoomController {
+}
---
+++ src/main/java/com/dailycodework/lakesidehotel/respository/BookedRoomRepository.java
@@ -0,0 +1,2 @@
+package com.dailycodework.lakesidehotel.respository;public interface BookedRoomRepository {
+}
